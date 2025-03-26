5c4e2b17112bb08162df5daa0b4145c7efa47774
HEO JIEUN <65994153+Heo-jieun@users.noreply.github.com>
2025-03-27 08:25:27 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.24 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
-    for(vector idxes : queries){
-        swap(arr[idxes[0]], arr[idxes[1]]);
+    for(vector query : queries){
+        swap(arr[query[0]], arr[query[1]]);
     }
     return arr;
 }
