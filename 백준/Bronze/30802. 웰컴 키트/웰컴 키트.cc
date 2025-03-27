3c09e96635ce0c1489c04e1b189767a00715c229
HEO JIEUN <65994153+Heo-jieun@users.noreply.github.com>
2025-03-27 14:19:28 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    // 변수 선언
+    int n, a, T, P ;
+    scanf("%d", &n); // 인원수 입력받기 
+    int size_arr[6];
+
+    // 배열 입력
+    for(int i = 0; i<6; i++){
+        scanf("%d", &size_arr[i]);
+    }
+
+    // 묶음 수 입력
+    scanf("%d%d", &T,&P);
+
+    // 펜 주문 구하기 
+    int pen_order = n / P;
+    int pen_one_order = n%P;
+
+    // 티셔츠 주문 구하기
+    int t_order = 0 ;
+    for(int size : size_arr){
+        t_order +=size / T;
+        if(size%T !=0) t_order++;
+    }
+    printf("%d\n%d %d\n", t_order, pen_order, pen_one_order);
+}
